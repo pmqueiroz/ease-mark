327cade2ab243a95763e6cee6facec6ea0415a1c
pmqueiroz <pedroqueiroz593@gmail.com>
2023-04-26 17:24:43 -0300

    chore: setup tests

---
+++ src/core/parse.spec.ts
@@ -0,0 +1,7 @@
+import { parse } from "./parse"
+
+describe('parse()', () => {
+   it('should be a function', () => {
+      expect(parse).toBeInstanceOf(Function)
+   })
+})
---
+++ src/core/parse.ts
@@ -0,0 +1,3 @@
+export const parse = (...args: any) => {
+   console.log(args)
+}
