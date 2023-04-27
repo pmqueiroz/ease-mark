59278b3f078b91442ded04a317aa6b5ec44fbd84
pmqueiroz <pedroqueiroz593@gmail.com>
2023-04-26 23:25:30 -0300

    chore: configure coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    globals: true
+    globals: true,
+    coverage: {
+      all: true,
+      100: true,
+      provider: 'c8',
+      include: ['src/**/*.ts'],
+      exclude: ['src/index.ts', '**/*.{spec,types}.ts'],
+    }
   },
 })
